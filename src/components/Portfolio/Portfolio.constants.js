01c6c6b730a0c83c09060119e738a7ee83b831ec
Анна-Марія <anna-mariia.kurhanevych.pz.2021@lpnu.ua>
2023-12-05 22:20:49 +0200

    Added info about Kurhanevych Maria

--- src/components/Portfolio/Portfolio.constants.js
+++ src/components/Portfolio/Portfolio.constants.js
@@ -1,6 +1,11 @@
+import photoKurhanevych from "../../images/kurhanevych-photo.png"
 
 export const teamInfo = [
-  
+  {
+    photo: photoKurhanevych,
+    initials: "Kurhanevych Maria",
+    position: "Full Stack Developer",
+  },
 ]
 
 export const dreamTeamTitle = "My dream team"
