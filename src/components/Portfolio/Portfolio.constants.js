6175d498abfa0e1f0405dd1f0774d64cd3348e96
Solomiia Lomnytska <lanalomn1111@gmail.com>
2023-12-05 22:37:57 +0200

    Added my picture and description to the page.

--- src/components/Portfolio/Portfolio.constants.js
+++ src/components/Portfolio/Portfolio.constants.js
@@ -1,6 +1,10 @@
-
+import solomiia_photo from '../../images/solomiia.png'
 export const teamInfo = [
-  
+    {
+    photo:solomiia_photo,
+    initials: "Solomiia Lomnytska",
+    position: "UI/UX Designer"
+    }
 ]
 
 export const dreamTeamTitle = "My dream team"
