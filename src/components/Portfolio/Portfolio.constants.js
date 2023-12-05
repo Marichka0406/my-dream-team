47c27118c06c7b83b55956c0538eba0bf84ff844
Yehor Lesnevych <yehor.lesnevych.pz.2021@lpnu.ua>
2023-12-05 22:44:37 +0200

    Addded Lesnevych Yehor info and photo

--- src/components/Portfolio/Portfolio.constants.js
+++ src/components/Portfolio/Portfolio.constants.js
@@ -1,10 +1,16 @@
 import photoKurhanevych from "../../images/kurhanevych-photo.png"
+import photoLesnevych from "../../images/lesnevych-photo.jpg"
 
 export const teamInfo = [
   {
     photo: photoKurhanevych,
     initials: "Kurhanevych Maria",
     position: "Full Stack Developer",
+  },
+  {
+    photo: photoLesnevych,
+    initials: "Lesnevych Yehor",
+    position: "QA",
   },
 ]
 
